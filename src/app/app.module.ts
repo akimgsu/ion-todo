c9e2ca1f85de1c720e4e42bdd7a8da48bc279a8c
Abraham Kim <akim92@gsu.edu>
2019-03-18 20:48:27 -0400

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,39 @@
+import { ArchivedTodosPage } from './../pages/archived-todos/archived-todos';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from "@angular/http";
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { TodoProvider } from '../providers/todo/todo';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ArchivedTodosPage
   ],
   imports: [
+    HttpModule,
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ArchivedTodosPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    TodoProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, reorderArray, ToastController } from 'ionic-angular';
+import { TodoProvider } from './../../providers/todo/todo';
+import { ArchivedTodosPage } from './../archived-todos/archived-todos';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,100 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public todos = [];
+  public reorderIsEnabled = false;
 
+  constructor(
+    private todoProvider: TodoProvider, 
+    private toastControllers: ToastController, 
+    public navCtrl: NavController, 
+    private alertController: AlertController) {
+      this.todos = this.todoProvider.getTodos();
+  }
+
+  archiveTodo(todoIndex){
+    this.todoProvider.archiveTodo(todoIndex);
+  }
+  
+  goToArchivePage(){
+    this.navCtrl.push(ArchivedTodosPage);
+  }
+  
+  toggleReorder(){
+    this.reorderIsEnabled = !this.reorderIsEnabled;
+  }
+  
+  itemReordered($event){
+    reorderArray(this.todos, $event)
+  }
+  
+  editTodo(todoIndex){
+    let editTodoAlert = this.alertController.create({
+      title: "Edit Todo",
+      inputs: [{
+        type: "text",
+        name: "editTodoInput",
+        value: this.todos[todoIndex]
+      }],
+      buttons: [
+        {
+          text: "Cancel"
+        },
+        {
+          text: "Edit Todo",
+          handler: (inputData) => {
+            let todoText;
+            todoText = inputData.editTodoInput;
+            this.todoProvider.editTodo(todoText, todoIndex);
+
+            editTodoAlert.onDidDismiss(()=>{
+              let addTodoToast = this.toastControllers.create({
+                message: "Edited Todo",
+                duration: 2000
+              });
+              addTodoToast.present();
+            });
+
+          }
+        }
+      ]
+    });
+    editTodoAlert.present();
+  }
+
+  openTodoAlert(){
+    let addTodoAlert = this.alertController.create({
+      title: "Add a Todo",
+      message: "Enter Your Todo",
+      inputs: [{
+        type: "text",
+        name: "addTodoInput"
+      }],
+      buttons: [
+        {
+          text: "Cancel"
+        },
+        {
+          text: "Add Todo",
+          handler: (inputData) => {
+            let todoText;
+            console.log("inputData", inputData.addTodoInput);
+            todoText = inputData.addTodoInput;
+            this.todoProvider.addTodo(todoText);
+
+            addTodoAlert.onDidDismiss(()=>{
+              let addTodoToast = this.toastControllers.create({
+                message: "Todo Added",
+                duration: 2000
+              });
+              addTodoToast.present();
+            });
+
+          }
+        }
+      ]
+    });
+    addTodoAlert.present();
   }
 
 }
